4f65be4f0cb7af49a7b985f1ba3d01aed73d5666
Olivier Trela <trelaolivier1@gmail.com>
2024-10-08 09:21:22 +0200

    Macierz zrobiona

--- Structures (no stl)/Matrix/Matrix/Matrix.h
+++ Structures (no stl)/Matrix/Matrix/Matrix.h
@@ -1,3 +1,58 @@
 //Matrix.h
 
 #pragma once
+
+#include <ostream>
+
+template <class T> T DefValue() {
+	throw std::exception();
+}
+
+template <class T> class Matrix {
+	size_t m;
+	size_t n;
+	T* data;
+
+public:
+	Matrix(const size_t m, const size_t n, const T& defValue = DefValue<T>()) : m(m), n(n), data(new T[m, n]) {
+		for (size_t i = 0; i < m; ++i) {
+			for (size_t j = 0; j < n; ++j) {
+				data[i, j] = defValue;
+			}
+		}
+			
+	}
+
+	~Matrix() noexcept {
+		delete[] data;
+	}
+
+	size_t Size_m() const noexcept {
+		return m;
+	}
+
+	size_t Size_n() const noexcept {
+		return n;
+	}
+
+	const T& operator[](const size_t index) const {
+		if (index >= m && index >= n) throw std::exception();
+		return data[index];
+	}
+
+	T& operator[](const size_t index) {
+		if (index >= m && index >= n) throw std::exception();
+		return data[index];
+	}
+};
+
+template<class T> std::ostream& operator<<(std::ostream& out, const Matrix<T>& mat) {
+	for (size_t i = 0; i < mat.Size_m(); ++i) {
+		for (size_t j = 0; j < mat.Size_n(); ++j) {
+			out << mat[i, j] << " ";
+		}
+		std::cout << std::endl;
+		
+	}
+	return out;
+}
--- Structures (no stl)/Matrix/Matrix/main.cpp
+++ Structures (no stl)/Matrix/Matrix/main.cpp
@@ -5,5 +5,14 @@
 #include "Matrix.h"
 
 int main() {
-	
+	Matrix<int> mat1(3, 4, 4);
+	std::cout << mat1 << std::endl;
+
+	Matrix<double> mat2(3, 4, 4);
+	std::cout << mat2 << std::endl;
+
+	Matrix<std::string> mat3(4, 4, "ok");
+	std::cout << mat3 << std::endl;
+
+
 }
